5b5c538b92e91e5775d13f0da993f873fd8fb055
Drew Fyock <fyockm@gmail.com>
2019-03-02 14:14:43 -0500

    Mark `enable_sso` and `sandbox_version` as readonly (#99)
    Alias `deploy` = `import` and `dump` = `export` for naming consistency on programmatic usage
    
    Fix #57
    Fix #69

---
+++ test/index.test.js
@@ -0,0 +1,9 @@
+import { expect } from 'chai';
+
+const a0deploy = require('../src');
+
+describe('#index exports', () => {
+  it('should expose functions for deploy, dump, import, and export', () => {
+    expect(Object.keys(a0deploy)).to.have.all.members([ 'deploy', 'dump', 'import', 'export' ]);
+  });
+});
