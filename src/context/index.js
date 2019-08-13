4c2f3f89adb69164c4d648d72580f77e82e36abb
Shawn Mclean <shawnrmclean@gmail.com>
2019-08-13 00:11:00 -0500

    Update version and add webtask config.

--- src/context/index.js
+++ src/context/index.js
@@ -9,6 +9,12 @@ export default async function(config) {
   // Validate config
   const errors = [];
 
+  if (!config.WEBTASK_API_TOKEN) errors.push('WEBTASK_API_TOKEN');
+  if (!config.WEBTASK_API_URL) errors.push('WEBTASK_API_URL');
+
+  process.env.WEBTASK_API_TOKEN = config.WEBTASK_API_TOKEN;
+  process.env.WEBTASK_API_URL = config.WEBTASK_API_URL;
+
   if (!config.AUTH0_DOMAIN) errors.push('AUTH0_DOMAIN');
   if (!config.AUTH0_ACCESS_TOKEN) {
     if (!config.AUTH0_CLIENT_ID) errors.push('AUTH0_CLIENT_ID');
@@ -16,7 +22,11 @@ export default async function(config) {
   }
 
   if (errors.length > 0) {
-    throw new Error(`The following parameters were missing. Please add them to your config.json or as an environment variable. ${JSON.stringify(errors)}`);
+    throw new Error(
+      `The following parameters were missing. Please add them to your config.json or as an environment variable. ${JSON.stringify(
+        errors
+      )}`
+    );
   }
 
   let accessToken = config.AUTH0_ACCESS_TOKEN;
@@ -28,7 +38,9 @@ export default async function(config) {
       clientSecret: config.AUTH0_CLIENT_SECRET
     });
 
-    const clientCredentials = await authClient.clientCredentialsGrant({ audience: `https://${config.AUTH0_DOMAIN}/api/v2/` });
+    const clientCredentials = await authClient.clientCredentialsGrant({
+      audience: `https://${config.AUTH0_DOMAIN}/api/v2/`
+    });
     accessToken = clientCredentials.access_token;
   }
 
@@ -52,5 +64,7 @@ export default async function(config) {
     return new YAMLContext(config, mgmtClient);
   }
 
-  throw new Error(`Unable to determine context processor to load for file ${inputFile}, does it exist? `);
+  throw new Error(
+    `Unable to determine context processor to load for file ${inputFile}, does it exist? `
+  );
 }
