33c337d424adbf1a4177f7388f899d560e28e4b1
Shu Shen <shu.shen@auth0.com>
2020-05-30 15:51:22 -0700

    pages: fix error when dumping error_page without html property

--- test/context/directory/pages.test.js
+++ test/context/directory/pages.test.js
@@ -16,12 +16,17 @@ const pages = {
   'guardian_multifactor.html': '<html>this is a page env ##env##</html>',
   'password_reset.json': '{ "name": "password_reset", "enabled": true }',
   'password_reset.html': '<html>this is a page env ##env##</html>',
-  'error_page.json': '{ "name": "error_page", "enabled": false }',
+  'error_page.json': '{ "name": "error_page", "url": "https://example.com/error", "show_log_link": false }',
   'error_page.html': '<html>this is a page env ##env##</html>'
 };
 
 const pagesTarget = [
-  { enabled: false, html: '<html>this is a page env test</html>', name: 'error_page' },
+  {
+    html: '<html>this is a page env test</html>',
+    name: 'error_page',
+    url: 'https://example.com/error',
+    show_log_link: false
+  },
   { enabled: true, html: '<html>this is a page env test</html>', name: 'guardian_multifactor' },
   { enabled: true, html: '<html>this is a page env test</html>', name: 'login' },
   { enabled: true, html: '<html>this is a page env test</html>', name: 'password_reset' }
@@ -71,6 +76,7 @@ describe('#directory context pages', () => {
     const dir = path.join(testDataDir, 'directory', 'pagesDump');
     cleanThenMkdir(dir);
     const context = new Context({ AUTH0_INPUT_FILE: dir }, mockMgmtClient());
+    const errorPageUrl = 'https://example.com/error';
 
     const htmlValidation = '<html>this is a env1 "env2"</html>';
 
@@ -78,7 +84,12 @@ describe('#directory context pages', () => {
       { html: htmlValidation, name: 'login' },
       { html: htmlValidation, name: 'password_reset' },
       { enabled: false, html: htmlValidation, name: 'guardian_multifactor' },
-      { html: htmlValidation, name: 'error_page' }
+      {
+        html: htmlValidation,
+        name: 'error_page',
+        url: errorPageUrl,
+        show_log_link: false
+      }
     ];
 
     await handler.dump(context);
@@ -94,7 +105,37 @@ describe('#directory context pages', () => {
     expect(loadJSON(path.join(pagesFolder, 'guardian_multifactor.json'))).to.deep.equal({ html: './guardian_multifactor.html', name: 'guardian_multifactor', enabled: false });
     expect(fs.readFileSync(path.join(pagesFolder, 'guardian_multifactor.html'), 'utf8')).to.deep.equal(htmlValidation);
 
-    expect(loadJSON(path.join(pagesFolder, 'error_page.json'))).to.deep.equal({ html: './error_page.html', name: 'error_page' });
+    expect(loadJSON(path.join(pagesFolder, 'error_page.json'))).to.deep.equal({
+      html: './error_page.html',
+      name: 'error_page',
+      url: errorPageUrl,
+      show_log_link: false
+    });
     expect(fs.readFileSync(path.join(pagesFolder, 'error_page.html'), 'utf8')).to.deep.equal(htmlValidation);
+  });
+
+  it('should dump error page without html', async () => {
+    const dir = path.join(testDataDir, 'directory', 'pagesDump');
+    cleanThenMkdir(dir);
+    const context = new Context({ AUTH0_INPUT_FILE: dir }, mockMgmtClient());
+    const errorPageUrl = 'https://example.com/error';
+
+    context.assets.pages = [
+      {
+        name: 'error_page',
+        url: errorPageUrl,
+        show_log_link: false
+      }
+    ];
+
+    await handler.dump(context);
+
+    const pagesFolder = path.join(dir, constants.PAGES_DIRECTORY);
+    expect(loadJSON(path.join(pagesFolder, 'error_page.json'))).to.deep.equal({
+      name: 'error_page',
+      url: errorPageUrl,
+      show_log_link: false
+    });
+    expect(fs.existsSync(path.join(pagesFolder, 'error_page.html'))).to.equal(false);
   });
 });
