ca60b7517414cdd5dfac5e9217bae8c4f5c4872e
Drew Fyock <fyockm@gmail.com>
2020-08-05 15:31:28 -0400

    feat: tenant dump handler

--- src/context/terraform/handlers/tenant.js
+++ src/context/terraform/handlers/tenant.js
@@ -1,13 +1,22 @@
+import { clearTenantFlags } from '../../../utils';
+
 async function parse(context) {
   throw new Error('Not Implemented' + context);
 }
 
 async function dump(context) {
-  throw new Error('Not Implemented' + context);
-}
+  const tenant = { ...(context.assets.tenant || {}) };
 
+  clearTenantFlags(tenant);
+
+  return {
+    type: 'auth0-tenant',
+    name: 'tenant',
+    content: tenant,
+  };
+}
 
 export default {
   parse,
-  dump
+  dump,
 };
