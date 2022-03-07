24e15cad1f335339d1eae0d3fbcc781fc7761764
Will Vedder <willvedder@jxmqxvcv5v.myfiosgateway.com>
2022-03-07 13:20:03 -0500

    Adding spacing

--- typescript-migration-progress.sh
+++ typescript-migration-progress.sh
@@ -5,10 +5,10 @@
 
 echo "- - - - - Typescript Migration - - - - - -"
 
-locJs=$((find ./src -name "*.js" -print0 | xargs -0 cat) | wc -l | xargs)
-locTs=$((find ./src -name "*.ts" -print0 | xargs -0 cat) | wc -l | xargs)
+locJs=$( (find ./src -name "*.js" -print0 | xargs -0 cat) | wc -l | xargs)
+locTs=$( (find ./src -name "*.ts" -print0 | xargs -0 cat) | wc -l | xargs)
 totalLoc=$(expr ${locJs} + ${locTs})
-percentLocMigrated=$(expr $(expr ${locTs} \* 100) / ${totalLoc})
+percentLocMigrated=$(expr $(${locTs} \* 100 | bc) / ${totalLoc})
 
 echo "${locTs} of ${totalLoc} (${percentLocMigrated}%) lines of code migrated to Typescript"
 
