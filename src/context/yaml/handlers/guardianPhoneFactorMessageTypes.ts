4d905649c9f168e6be1378bfdfaf92f3db8347a7
Will Vedder <willvedder@jxmqxvcv5v.myfiosgateway.com>
2022-03-22 13:47:16 -0400

    Fixing types of guardian phone factor message types

--- src/context/yaml/handlers/guardianPhoneFactorMessageTypes.ts
+++ src/context/yaml/handlers/guardianPhoneFactorMessageTypes.ts
@@ -1,13 +1,13 @@
 import { YAMLHandler, Context } from '.'
 
 type ParsedGuardianFactorMessageTypes = {
-  guardianPhoneFactorMessageTypes: unknown[]
+  guardianPhoneFactorMessageTypes: unknown
 }
 
 async function parseAndDump(context: Context): Promise<ParsedGuardianFactorMessageTypes> {
   // nothing to do, set default if empty
   return {
-    guardianPhoneFactorMessageTypes: [...context.assets.guardianPhoneFactorMessageTypes || []]
+    guardianPhoneFactorMessageTypes: { ...context.assets.guardianPhoneFactorMessageTypes || {} }
   };
 }
 
