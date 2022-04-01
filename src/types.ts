43f41ed307007fc85014af08cea16585103e0aa4
Will Vedder <willvedder@jxmqxvcv5v.myfiosgateway.com>
2022-04-01 11:11:07 -0400

    Removing capital L

--- src/types.ts
+++ src/types.ts
@@ -1,4 +1,4 @@
-type APICLientBaseFunctions = {
+type APIClientBaseFunctions = {
     getAll: (arg0: { checkpoint?: boolean, is_global?: boolean, paginate?: boolean, include_totals?: boolean, id?: string }) => Promise<Asset[]>
     create: (arg0: { id: string }) => Promise<Asset>
     update: (arg0: {}, arg1: Asset) => Promise<Asset>
@@ -14,13 +14,13 @@ export type Auth0APIClient = {
             promise: () => Promise<void>
         }
     }
-    actions: APICLientBaseFunctions & {
+    actions: APIClientBaseFunctions & {
         deploy: ({ id: string }) => Promise<void>
         getAllTriggers: () => Promise<{ triggers: Asset[] }>
         getTriggerBindings: ({ trigger_id: string }) => Promise<{ bindings: Asset[] }>
         updateTriggerBindings: ({ trigger_id: string }, { bindings: Object }) => Promise<{ bindings: Asset[] }>
     }
-    attackProtection: APICLientBaseFunctions & {
+    attackProtection: APIClientBaseFunctions & {
         getBreachedPasswordDetectionConfig: () => Promise<Asset>
         getBruteForceConfig: () => Promise<Asset>
         getSuspiciousIpThrottlingConfig: () => Promise<Asset>
@@ -28,30 +28,30 @@ export type Auth0APIClient = {
         updateSuspiciousIpThrottlingConfig: ({ }, arg1: Asset) => Promise<void>
         updateBruteForceConfig: ({ }, arg1: Asset) => Promise<void>
     }
-    branding: APICLientBaseFunctions & {
+    branding: APIClientBaseFunctions & {
         getSettings: () => Promise<Asset>
         getUniversalLoginTemplate: () => Promise<Asset>
         updateSettings: ({ }, Asset) => Promise<void>
         setUniversalLoginTemplate: ({ }, Asset) => Promise<void>
     }
-    clients: APICLientBaseFunctions
-    clientGrants: APICLientBaseFunctions
-    connections: APICLientBaseFunctions & {
+    clients: APIClientBaseFunctions
+    clientGrants: APIClientBaseFunctions
+    connections: APIClientBaseFunctions & {
         get: (arg0: Asset) => Promise<Asset>
         getAll: (arg0: { strategy: 'auth0', checkpoint?: boolean, is_global?: boolean, paginate?: boolean, include_totals?: boolean, id?: string }) => Promise<Asset[]>
     }
-    customDomains: APICLientBaseFunctions & {
+    customDomains: APIClientBaseFunctions & {
         getAll: () => Promise<Asset[]>
     }
-    emailProvider: APICLientBaseFunctions & {
+    emailProvider: APIClientBaseFunctions & {
         delete: () => Promise<void>
         get: (arg0: Asset) => Promise<Asset>
         configure: (arg0: Object, arg1: Object) => Promise<Asset>
     }
-    emailTemplates: APICLientBaseFunctions & {
+    emailTemplates: APIClientBaseFunctions & {
         get: (arg0: Asset) => Promise<Asset>
     }
-    guardian: APICLientBaseFunctions & {
+    guardian: APIClientBaseFunctions & {
         getFactorProvider: (arg0: Asset) => Promise<Asset>
         updateFactorProvider: (arg0: {}, arg1: Asset) => Promise<void>
         getFactors: () => Promise<Asset[]>
@@ -65,18 +65,18 @@ export type Auth0APIClient = {
         getPhoneFactorMessageTypes: () => Promise<Asset[]>
         updatePhoneFactorSelectedProvider: (arg0: {}, arg1: Asset) => Promise<void>
     }
-    hooks: APICLientBaseFunctions & {
+    hooks: APIClientBaseFunctions & {
         get: ({ id: string }) => Promise<Asset>
         removeSecrets: (arg0: {}, arg1: Asset) => Promise<void>
         updateSecrets: (arg0: {}, arg1: Asset) => Promise<void>
         getSecrets: ({ id: string }) => Promise<Promise<Asset[]>>
         addSecrets: (arg0: {}, arg1: Asset) => Promise<void>
     }
-    migrations: APICLientBaseFunctions & {
+    migrations: APIClientBaseFunctions & {
         getMigrations: () => Promise<{ flags: Asset[] }>
         updateMigrations: (arg0: { flags: Asset[] }) => Promise<void>
     }
-    organizations: APICLientBaseFunctions & {
+    organizations: APIClientBaseFunctions & {
         updateEnabledConnection: (arg0: {}, arg1: Asset) => Promise<void>
         addEnabledConnection: (arg0: {}, arg1: Asset) => Promise<void>
         removeEnabledConnection: (arg0: Asset) => Promise<void>
@@ -84,26 +84,26 @@ export type Auth0APIClient = {
             get: (arg0: Asset) => Promise<Asset>
         }
     }
-    prompts: APICLientBaseFunctions & {
+    prompts: APIClientBaseFunctions & {
         getSettings: () => Promise<Asset[]>
         updateSettings: (arg0: {}, arg1: Asset) => Promise<void>
     }
-    resourceServers: APICLientBaseFunctions,
-    roles: APICLientBaseFunctions & {
-        permissions: APICLientBaseFunctions & {
+    resourceServers: APIClientBaseFunctions,
+    roles: APIClientBaseFunctions & {
+        permissions: APIClientBaseFunctions & {
             delete: (arg0: { id: string }, arg1: { permissions: Asset[] }) => Promise<void>
             create: (arg0: { id: string }, arg1: { permissions: Asset[] }) => Promise<Asset>
         }
     }
-    rules: APICLientBaseFunctions,
-    rulesConfigs: APICLientBaseFunctions & {
+    rules: APIClientBaseFunctions,
+    rulesConfigs: APIClientBaseFunctions & {
         getAll: () => Promise<Asset[]>
     }
-    tenant: APICLientBaseFunctions & {
+    tenant: APIClientBaseFunctions & {
         getSettings: () => Promise<Asset>
         updateSettings: (arg0: Asset) => Promise<void>
     }
-    triggers: APICLientBaseFunctions & {
+    triggers: APIClientBaseFunctions & {
         getTriggerBindings: () => Promise<Asset>
     }
     updateRule: (arg0: { id: string }, arg1: Asset) => Promise<Asset>
