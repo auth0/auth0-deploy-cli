d5e86fe0200969b717f0ece8606b274a0c1453f9
Will Vedder <willvedder@jxmqxvcv5v.myfiosgateway.com>
2022-07-25 16:28:25 -0400

    Fixing invalid JSON issue

--- test/e2e/e2e-cli.sh
+++ test/e2e/e2e-cli.sh
@@ -9,7 +9,7 @@ CONFIG_FILE="./config-cli-e2e.json"
 echo "{
   \"AUTH0_DOMAIN\": \"$AUTH0_E2E_TENANT_DOMAIN\",
   \"AUTH0_CLIENT_ID\": \"$AUTH0_E2E_CLIENT_ID\",
-  \"AUTH0_CLIENT_SECRET\": \"$AUTH0_E2E_CLIENT_SECRET\",
+  \"AUTH0_CLIENT_SECRET\": \"$AUTH0_E2E_CLIENT_SECRET\"
 }" > $CONFIG_FILE
 
 node lib/index.js export --env=false --output_folder=$WORK_DIR --format=yaml -c=$CONFIG_FILE
