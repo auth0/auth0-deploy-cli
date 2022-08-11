5fa84ea5fdde16b7d051f7f725d7eb238911a9ac
Will Vedder <will.vedder@okta.com>
2022-08-11 16:52:38 -0400

    Removing rimraf and updating script

--- test/e2e/e2e-cli.sh
+++ test/e2e/e2e-cli.sh
@@ -1,10 +1,14 @@
 #!/bin/sh
 
-npm run build
-
 WORK_DIR="./local/cli"
 CONFIG_FILE="./config-cli-e2e.json"
 
+npm ci #Install all dependencies to allow for building
+npm run build
+npm ci --omit=dev #Remove dev dependencies
+
+TARBALL_PATH=$(npm pack)
+npm install $TARBALL_PATH
 
 echo "{
   \"AUTH0_DOMAIN\": \"$AUTH0_E2E_TENANT_DOMAIN\",
@@ -12,10 +16,10 @@ echo "{
   \"AUTH0_CLIENT_SECRET\": \"$AUTH0_E2E_CLIENT_SECRET\"
 }" > $CONFIG_FILE
 
-node lib/index.js export --env=false --output_folder=$WORK_DIR --format=yaml -c=$CONFIG_FILE
+npx a0deploy export --env=false --output_folder=$WORK_DIR --format=yaml -c=$CONFIG_FILE
 
 echo "-------- Beginning deploy/import phase --------"
 
-node lib/index.js import --env=false --input_file=$WORK_DIR --format=yaml -c=$CONFIG_FILE
+npx a0deploy import --env=false --input_file=$WORK_DIR --format=yaml -c=$CONFIG_FILE
 
 rm $CONFIG_FILE
