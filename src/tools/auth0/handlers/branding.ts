0b688d2e1b9033497bf0c798efe65eb7ab93a47c
Will Vedder <will.vedder@okta.com>
2022-10-12 17:22:25 -0400

    Returning non-template branding settings for YAML branding handler

--- src/tools/auth0/handlers/branding.ts
+++ src/tools/auth0/handlers/branding.ts
@@ -69,14 +69,12 @@ export default class BrandingHandler extends DefaultHandler {
   }
 
   async processChanges(assets: Assets) {
-    const { branding } = assets;
-
     // quit early if there's no branding to process.
-    if (!branding) return;
+    if (!assets.branding) return;
 
     // remove templates, we only want top level branding settings for this API call
-    const brandingSettings = { ...branding };
-    delete brandingSettings.templates;
+    const { templates, ...brandingSettings } = assets.branding;
+
     // Do nothing if not set
     if (brandingSettings && Object.keys(brandingSettings).length) {
       await this.client.branding.updateSettings({}, brandingSettings);
@@ -85,8 +83,8 @@ export default class BrandingHandler extends DefaultHandler {
     }
 
     // handle templates
-    if (branding.templates && branding.templates.length) {
-      const unknownTemplates = branding.templates
+    if (templates && templates.length) {
+      const unknownTemplates = templates
         .filter((t) => !constants.SUPPORTED_BRANDING_TEMPLATES.includes(t.template))
         .map((t) => t.template);
       if (unknownTemplates.length) {
@@ -98,7 +96,7 @@ export default class BrandingHandler extends DefaultHandler {
         );
       }
 
-      const templateDefinition = branding.templates.find(
+      const templateDefinition = templates.find(
         (t) => t.template === constants.UNIVERSAL_LOGIN_TEMPLATE
       );
       if (templateDefinition && templateDefinition.body) {
@@ -107,7 +105,7 @@ export default class BrandingHandler extends DefaultHandler {
           { template: templateDefinition.body }
         );
         this.updated += 1;
-        this.didUpdate(branding.templates);
+        this.didUpdate(templates);
       }
     }
   }
