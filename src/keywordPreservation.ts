ec3d24887146e59e0c9ed76cbf8233a5247692e4
Will Vedder <willvedd@gmail.com>
2023-02-14 11:38:07 -0500

    DXCDT-373: Address notation for keyword preservation (#741)
    * Adding shouldFieldBePreserved function
    
    * Adding getPreservableFieldsFromAssets function with test
    
    * Adding array keyword replace case
    
    * Adding undefined and null check
    
    * Adding address notation to traversal
    
    * Adding address notation lookup
    
    * Adding stronger tests, cleaning up code
    
    * Adding more context
    
    * Adding address notation to traversal
    
    * Adding address notation lookup
    
    * Adding stronger tests, cleaning up code
    
    * Adding more context
    
    * Removing unused package
    
    * Removing unused package
    
    ---------
    
    Co-authored-by: Will Vedder <will.vedder@okta.com>

--- src/keywordPreservation.ts
+++ src/keywordPreservation.ts
@@ -1,3 +1,4 @@
+import { get as getByDotNotation } from 'dot-prop';
 import { KeywordMappings } from './types';
 import { keywordReplaceArrayRegExp, keywordReplaceStringRegExp } from './tools/utils';
 
@@ -15,18 +16,32 @@ export const shouldFieldBePreserved = (
 
 export const getPreservableFieldsFromAssets = (
   asset: any,
+  address: string,
   keywordMappings: KeywordMappings
 ): string[] => {
   if (typeof asset === 'string') {
     if (shouldFieldBePreserved(asset, keywordMappings)) {
-      return [asset];
+      return [address];
     }
     return [];
   }
+
+  const shouldRenderDot = address !== '';
+
   if (Array.isArray(asset)) {
     return asset
       .map((arrayItem) => {
-        return getPreservableFieldsFromAssets(arrayItem, keywordMappings);
+        // Using the `name` field as the primary unique identifier for array items
+        // TODO: expand the available identifier fields to encompass objects that lack name
+        const hasIdentifier = arrayItem.name !== undefined;
+
+        if (!hasIdentifier) return [];
+
+        return getPreservableFieldsFromAssets(
+          arrayItem,
+          `${address}${shouldRenderDot ? '.' : ''}[name=${arrayItem.name}]`,
+          keywordMappings
+        );
       })
       .flat();
   }
@@ -36,9 +51,54 @@ export const getPreservableFieldsFromAssets = (
         const value = asset[key];
 
         if (value === undefined || value === null) return [];
-        return getPreservableFieldsFromAssets(value, keywordMappings);
+
+        return getPreservableFieldsFromAssets(
+          value,
+          `${address}${shouldRenderDot ? '.' : ''}${key}`,
+          keywordMappings
+        );
       })
       .flat();
   }
   return [];
+};
+
+// getAssetsValueByAddress returns a value for an arbitrary data structure when
+// provided an "address" of that value. This address is similar to JS object notation
+// with the exception of identifying array items by a unique property instead of order.
+// Example:
+// Object: `{ actions: [ { name: "action-1", code: "..."}] }`
+// Address: `.actions[name=action-1].code`
+export const getAssetsValueByAddress = (address: string, assets: any): any => {
+  //Look ahead and see if the address path only contains dots (ex: `tenant.friendly_name`)
+  //if so the address is trivial and can use the dot-prop package to return the value
+
+  const isTrivialAddress = address.indexOf('[') === -1;
+  if (isTrivialAddress) {
+    return getByDotNotation(assets, address);
+  }
+
+  // It is easier to handle an address piece-by-piece by
+  // splitting on the period into separate "directions"
+  const directions = address.split('.');
+
+  // If the the next directions are the proprietary array syntax (ex: `[name=foo]`)
+  // then perform lookup against unique array-item property
+  if (directions[0].charAt(0) === '[') {
+    const identifier = directions[0].substring(1, directions[0].length - 1).split('=')[0];
+    const identifierValue = directions[0].substring(1, directions[0].length - 1).split('=')[1];
+
+    if (assets === undefined) return undefined;
+
+    const target = assets.find((item: any) => {
+      return item[identifier] === identifierValue;
+    });
+
+    return getAssetsValueByAddress(directions.slice(1).join('.'), target);
+  }
+
+  return getAssetsValueByAddress(
+    directions.slice(1).join('.'),
+    getByDotNotation(assets, directions[0])
+  );
 };
