ec3ece5d82e35c01461d3e9ddeab95948f6506a7
Rajat Bajaj <rajat.bajaj@okta.com>
2024-07-11 14:10:27 +0530

    Updated linting

--- src/commands/export.ts
+++ src/commands/export.ts
@@ -61,14 +61,15 @@ export default async function exportCMD(params: ExportParams) {
   await context.dump();
   log.info('Export Successful');
 
-  log.info(`================================================
-            ======= Help us improve Auth0 Deploy CLI =======
-            ================================================
-            We're on a mission to make Auth0 Deploy CLI the best it can be, and we need YOUR help.
-            We've put together a brief survey to understand how you use Deploy CLI, what you love about it, and where you think we can do better.
+  log.info(`
+  ================================================
+  ======= Help us improve Auth0 Deploy CLI =======
+  ================================================
+  We're on a mission to make Auth0 Deploy CLI the best it can be, and we need YOUR help.
+  We've put together a brief survey to understand how you use Deploy CLI, what you love about it, and where you think we can do better.
 
-            ===> https://www.surveymonkey.com/r/LZKMPFN  <===
+  ===> https://www.surveymonkey.com/r/LZKMPFN  <===
 
-            Thank you for helping us make Auth0 Deploy CLI better for everyone!
-            ================================================`);
+  Thank you for helping us make Auth0 Deploy CLI better for everyone!
+  ================================================`);
 }
