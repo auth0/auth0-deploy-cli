1c2929f00df047acf53983cf205b1d75dbabced4
kushalshit27 <43465488+kushalshit27@users.noreply.github.com>
2024-09-04 16:33:57 +0530

    DXCDT-701:added check AUTH0_PRESERVE_KEYWORDS flag

--- src/context/yaml/handlers/clientGrants.ts
+++ src/context/yaml/handlers/clientGrants.ts
@@ -12,19 +12,29 @@ async function parse(context: YAMLContext): Promise<ParsedClientGrants> {
 
   if (!clientGrants) return { clientGrants: null };
 
-  // can not use client from context.assets because it does not have the client_id
-  const clients = await context.mgmtClient.clients.getAll({
-    paginate: true,
-    include_totals: true,
-  });
+  const shouldPreserveKeywords =
+    // @ts-ignore because the string=>boolean conversion may not have happened if passed-in as env var
+    context.config.AUTH0_PRESERVE_KEYWORDS === 'true' ||
+    context.config.AUTH0_PRESERVE_KEYWORDS === true;
 
-  return {
-    clientGrants: clientGrants.map((grant) => {
-      const dumpGrant = { ...grant };
-      dumpGrant.client_id = convertClientNameToId(dumpGrant.client_id, clients || []);
-      return dumpGrant;
-    }),
-  };
+  if (shouldPreserveKeywords) {
+    // AUTH0_PRESERVE_KEYWORDS need client id instead for client name
+    // here can not use client from context.assets because it does not have the client_id
+    const clients = await context.mgmtClient.clients.getAll({
+      paginate: true,
+      include_totals: true,
+    });
+
+    return {
+      clientGrants: clientGrants.map((grant) => {
+        const dumpGrant = { ...grant };
+        dumpGrant.client_id = convertClientNameToId(dumpGrant.client_id, clients || []);
+        return dumpGrant;
+      }),
+    };
+  }
+
+  return { clientGrants };
 }
 
 async function dump(context: YAMLContext): Promise<ParsedClientGrants> {
