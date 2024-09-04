f951152c68d37fe78f4bb2e8393fb095b0681469
kushalshit27 <43465488+kushalshit27@users.noreply.github.com>
2024-09-04 17:08:46 +0530

    DXCDT-701: unit test added should process client grants with AUTH0_PRESERVE_KEYWORDS: true

--- test/context/yaml/clientGrants.test.js
+++ test/context/yaml/clientGrants.test.js
@@ -37,6 +37,40 @@ describe('#YAML context client grants', () => {
     expect(context.assets.clientGrants).to.deep.equal(target);
   });
 
+  it('should process client grants with AUTH0_PRESERVE_KEYWORDS: true', async () => {
+    const dir = path.join(testDataDir, 'yaml', 'clientGrants');
+    cleanThenMkdir(dir);
+
+    const yaml = `
+    clientGrants:
+      - client_id: "my_client_id"
+        name: "My M2M"
+        audience: "https://##ENV##.myapp.com/api/v1"
+        scope:
+          - "update:account"
+    `;
+    const yamlFile = path.join(dir, 'config.yaml');
+    fs.writeFileSync(yamlFile, yaml);
+
+    const target = [
+      {
+        audience: 'https://test.myapp.com/api/v1',
+        client_id: 'my_client_id',
+        name: 'My M2M',
+        scope: ['update:account'],
+      },
+    ];
+
+    const config = {
+      AUTH0_INPUT_FILE: yamlFile,
+      AUTH0_KEYWORD_REPLACE_MAPPINGS: { ENV: 'test' },
+      AUTH0_PRESERVE_KEYWORDS: true,
+    };
+    const context = new Context(config, mockMgmtClient());
+    await context.loadAssetsFromLocal();
+    expect(context.assets.clientGrants).to.deep.equal(target);
+  });
+
   it('should dump client grants', async () => {
     const context = new Context({ AUTH0_INPUT_FILE: './test.yml' }, mockMgmtClient());
     const clientGrants = [
