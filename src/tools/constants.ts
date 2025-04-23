8148e0f515d607231315a875428a2a0c5d003079
ramya18101 <anusriankisetty@gmail.com>
2025-04-23 17:14:16 +0530

    Support new screens for ACUL

--- src/tools/constants.ts
+++ src/tools/constants.ts
@@ -235,7 +235,16 @@ const constants = {
       'reset-password-mfa-email-challenge',
       'reset-password-mfa-otp-challenge',
       'reset-password-mfa-push-challenge-push',
-      'reset-password-mfa-sms-challenge'
+      'reset-password-mfa-sms-challenge',
+      'reset-password-mfa-phone-challenge',
+      'reset-password-mfa-voice-challenge',
+      'reset-password-mfa-recovery-code-challenge'
+    ],
+    'device-flow': [
+      'device-code-activation',
+      'device-code-activation-allowed',
+      'device-code-activation-denied',
+      'device-code-confirmation'
     ],
     mfa: [
       'mfa-detect-browser-capabilities',
@@ -251,6 +260,8 @@ const constants = {
       'mfa-push-list',
       'mfa-push-welcome',
     ],
+    'mfa-phone':['mfa-phone-challenge', 'mfa-phone-enrollment'],
+    'mfa-voice': ['mfa-voice-challenge', 'mfa-voice-enrollment'],
     'invitation': ['accept-invitation'],
     'organizations': ['organization-selection', 'organization-picker'],
     'mfa-otp': [
@@ -258,6 +269,8 @@ const constants = {
       'mfa-otp-enrollment-code',
       'mfa-otp-enrollment-qr'
     ],
+    'mfa-recovery-code': ['mfa-recovery-code-challenge', 'mfa-recovery-code-enrollment'],
+    'common': ['redeem-ticket'],
 
   } as PromptScreenMapping,
 };
