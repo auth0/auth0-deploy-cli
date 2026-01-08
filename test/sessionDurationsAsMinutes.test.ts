38239c5017d36c4bee51fb65ddd9f307884a552c
Palash Gupta <palashgdev@gmail.com>
2026-01-08 20:00:08 +0530

    fix: add support for ephemeral session lifetimes in tenant handling (#1261)
    feat: add support for ephemeral session lifetimes in tenant handling

--- test/sessionDurationsAsMinutes.test.ts
+++ test/sessionDurationsAsMinutes.test.ts
@@ -1,5 +1,4 @@
 import { expect } from 'chai';
-
 import { sessionDurationsToMinutes } from '../src/sessionDurationsToMinutes';
 
 describe('#sessionDurationsToMinutes', () => {
@@ -8,19 +7,25 @@ describe('#sessionDurationsToMinutes', () => {
 
     expect(sessionDurations.idle_session_lifetime_in_minutes).to.be.undefined;
     expect(sessionDurations.session_lifetime_in_minutes).to.be.undefined;
+    expect(sessionDurations.idle_ephemeral_session_lifetime_in_minutes).to.be.undefined;
+    expect(sessionDurations.ephemeral_session_lifetime_in_minutes).to.be.undefined;
   });
 
-  it('should convert session durations to minutes durations are defined', () => {
+  it('should convert all session durations to minutes when all are defined', () => {
     const sessionDurations = sessionDurationsToMinutes({
       session_lifetime: 1,
-      idle_session_lifetime: 1,
+      idle_session_lifetime: 2,
+      ephemeral_session_lifetime: 1.5,
+      idle_ephemeral_session_lifetime: 0.5,
     });
 
-    expect(sessionDurations.idle_session_lifetime_in_minutes).to.equal(60);
     expect(sessionDurations.session_lifetime_in_minutes).to.equal(60);
+    expect(sessionDurations.idle_session_lifetime_in_minutes).to.equal(120);
+    expect(sessionDurations.ephemeral_session_lifetime_in_minutes).to.equal(90);
+    expect(sessionDurations.idle_ephemeral_session_lifetime_in_minutes).to.equal(30);
   });
 
-  it('should convert session durations to minutes durations are partially defined', () => {
+  it('should convert session durations to minutes when durations are partially defined', () => {
     const sessionDurations1 = sessionDurationsToMinutes({
       session_lifetime: undefined,
       idle_session_lifetime: 1,
@@ -28,6 +33,7 @@ describe('#sessionDurationsToMinutes', () => {
 
     expect(sessionDurations1.idle_session_lifetime_in_minutes).to.equal(60);
     expect(sessionDurations1.session_lifetime_in_minutes).to.be.undefined;
+    expect(sessionDurations1.ephemeral_session_lifetime_in_minutes).to.be.undefined;
 
     const sessionDurations2 = sessionDurationsToMinutes({
       session_lifetime: 1,
@@ -36,5 +42,13 @@ describe('#sessionDurationsToMinutes', () => {
 
     expect(sessionDurations2.idle_session_lifetime_in_minutes).to.be.undefined;
     expect(sessionDurations2.session_lifetime_in_minutes).to.equal(60);
+
+    const sessionDurations3 = sessionDurationsToMinutes({
+      ephemeral_session_lifetime: 1,
+    });
+
+    expect(sessionDurations3.ephemeral_session_lifetime_in_minutes).to.equal(60);
+    expect(sessionDurations3.idle_ephemeral_session_lifetime_in_minutes).to.be.undefined;
+    expect(sessionDurations3.session_lifetime_in_minutes).to.be.undefined;
   });
 });
